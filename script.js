f8ce92b8e61bdf9e74bcb34449f5854a4134b55b
RonorayraSkyLine <57844233+RonorayraSkyLine@users.noreply.github.com>
2022-02-16 10:38:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$(".dws-form").on("click", ".tab", function() {
+    $(".dws-form").find(".active").removeClass("active");
+$(this).addClass("active");
+$(".tab-form").eq($(this).index()).addClass("active");
+});
+
